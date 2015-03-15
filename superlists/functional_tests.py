04dbae32c131938c93beb6f9b130f38dfffc0855
sfall <sfall@my.tnstate.edu>
2015-03-14 22:16:18 -0500

    Developed POST unit test and revised typo in functional tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -27,7 +27,7 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # She is invited to enter a to-do iten straight away
+        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from lists.models import Item
 
-# Create your views here.
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(request, 'home.html', {
         'new_item_text': request.POST.get('item_text', ''),
     })
