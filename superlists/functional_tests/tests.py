196ccf1eb00f7e872afbd5f7fd33bbc727cf2e34
sfall <sfall@my.tnstate.edu>
2015-05-16 18:13:05 -0500

    Use Bootstrap to improve layout

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,8 +1,8 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.exec_path = "C:\phantomjs-1.9.8-windows\phantomjs.exe"
         self.browser = webdriver.PhantomJS(executable_path=self.exec_path)
